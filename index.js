b0e72f6fb2ebf87450ea6f8a59a2f04071362a35
Isc740 <isaazcantillo@gmail.com>
2024-11-08 19:21:30 -0500

    add playRound function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function getHumanChoice() {
-    let choice = parseInt(prompt("Type your choice\n 1: Rock\n2:Paper\n3:Scissors"));
+    const choice = parseInt(
+        prompt("Type your choice\n 1: Rock\n2:Paper\n3:Scissors"),
+    );
     if (choice !== 1 && choice !== 2 && choice !== 3) {
         alert("Valor ingresado no valido!");
         getHumanChoice();
@@ -7,4 +9,23 @@ function getHumanChoice() {
     return choice;
 }
 
-const getComputerChoice = () => Math.floor((Math.random() * 3)) + 1;
+const getComputerChoice = () => Math.floor(Math.random() * 3) + 1;
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (
+        humanChoice === 1 && computerChoice === 2 ||
+        humanChoice === 2 && computerChoice === 3 ||
+        humanChoice === 3 && computerChoice === 1
+    ) {
+        computerScore++;
+    } else if (
+        humanChoice === 2 && computerChoice === 1 ||
+        humanChoice === 3 && computerChoice === 2 ||
+        humanChoice === 1 && computerChoice === 3
+    ) {
+        humanScore++;
+    }
+}
